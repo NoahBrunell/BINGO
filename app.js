8ff3c5ec7b2b39a12b8166af96b6df077a99750e
NoahBrunell <brunellnoah@gmail.com>
2022-10-14 13:20:37 +0300

    Version 2 - Prevented duplicate numbers

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const numbersDiv = document.getElementById("numbers")
 
+const numberLogs = []
+
+function generateNumber() {
+    const numberLog = Math.floor(Math.random() * 75) + 1;
+
+    if (numberLogs.includes(numberLog)) {
+        return generateNumber()
+
+    } else {
+        numberLogs.push(numberLog)
+        return numberLog
+    }
+}
+
+
+
 function toggleNumber(el) {
     // Check if number is already "locked"
     if (el.classList == "locked") {
@@ -16,14 +32,15 @@ function toggleNumber(el) {
     }
 }
 
-// Create 40 buttons inside every div
+// Create 25 buttons inside the div
 for (let i = 1; i < 26; i++) {
     const newButton = document.createElement("button");
 
     // Set id on button
     newButton.id = "button" + i;
     // Set text inside button
-    newButton.innerText = Math.floor(Math.random() * 75) + 1;
+    newButton.innerText = generateNumber()
+
     // Set onclick on button
     newButton.onclick = function () {
         toggleNumber(this);
@@ -32,5 +49,8 @@ for (let i = 1; i < 26; i++) {
     // Add button inside div
     numbersDiv.appendChild(newButton);
 }
+
+
+
 
 document.getElementById("button13").innerHTML = "<img src='https://editzstock.com/wp-content/uploads/2022/05/Crown-PNG-image-with-transparent-background-1.png'>"
